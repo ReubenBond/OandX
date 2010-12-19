a366cb34b6652b7b9777469710c014d0c3ac6710
Reuben Bond <reuben.bond@gmail.com>
2010-12-20 00:23:34 +1100

    Working game... needs polish, modularity, and more

--- oandx.go
+++ oandx.go
@@ -14,8 +14,12 @@ const APP_VERSION = "0.1"
 const SCREEN_WIDTH = 224
 const SCREEN_HEIGHT = 224
 //var WORKING_DIR, _ = os.Getwd()
-var GFX_DIR string = "gfx"
+var GFX_DIR string = "gfx" // Graphics
+var FONT_DIR string = "font" // Fonts
+var MUSIC_DIR string = "music" // Background music
+var SFX_DIR string = "sfx" // Sound effects
 
+//TODO: Separate resource manager into separate package
 // Resource manager
 var gfxResources = make(map[string]*sdl.Surface)
 
@@ -35,11 +39,11 @@ func GetImage(fileName string) (image *sdl.Surface, ok bool) {
 			ok = true
 		}
 	}
-	return image, ok
+	return
 }
 
 // Get an image or panic
-func MustGetImage(fileName string) *sdl.Surface {
+func MustGetImage(fileName string) (image *sdl.Surface) {
 	image, ok := GetImage(fileName)
 	if !ok {
 		panic("MustGetImage:"+" Failed to load "+fileName)
@@ -66,12 +70,42 @@ func FreeImage(image *sdl.Surface) {
 	}
 }
 
+// Describes a font uniquely
+var fontResources = make(map[string]map[int]*ttf.Font)
+func GetFont(fileName string, size int) (font *ttf.Font, ok bool) {
+	if font, ok = fontResources[fileName][size]; !ok {
+		font = ttf.OpenFont(fileName, size)
+		if font != nil {
+			fontResources[fileName][size] = font
+			ok = true
+		}
+	}
+	return
+}
+
+func MustGetFont(fileName string, size int) (font *ttf.Font) {
+	font, ok := GetFont(fileName, size);
+	if !ok {
+		panic("MustGetFont"+" Failed to load "+fileName+" with specified size")
+	}
+	return
+}
+
+//TODO: FreeFont*() functions
+
 // Free all managed resources
 func FreeResources() {
 	// Free all managed image resources
 	for k, v := range gfxResources {
 		v.Free()
 		gfxResources[k] = nil, false
+	}
+	// Close all fonts
+	for _, sizes := range fontResources {
+		for size, font := range sizes {
+			font.Close()
+			sizes[size] = nil, false
+		}
 	}
 }
 
@@ -219,7 +253,7 @@ func init() {
 	}
 	sdl.EnableUNICODE(1)
 	sdl.WM_SetCaption("OandX - "+APP_VERSION, "")
-	screen = sdl.SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, sdl.RESIZABLE)
+	screen = sdl.SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, sdl.RESIZABLE & sdl.OPENGL)
 	if screen == nil {
 		panic(sdl.GetError())
 	}
@@ -228,10 +262,13 @@ func init() {
 	BackgroundImageName = path.Join(GFX_DIR, BackgroundImageName)
 	background = MustGetImage(BackgroundImageName)
 	for i, rsrc := range TileImageNames {
+		// Resource comes from graphics directory
 		TileImageNames[i] = path.Join(GFX_DIR, rsrc)
+		// Load resource or die
 		tiles[i] = MustGetImage(TileImageNames[i])
-		fmt.Println("Tile", i, "is", TileImageNames[i])
 	}
+	// Set the Window Manager icon for the game window
+	sdl.WM_SetIcon(tiles[Cross], nil)
 }
 
 func deinit() {
@@ -243,22 +280,9 @@ func main() {
 	// Recall that call to init() is implicit
 	// Clean up when we quit
 	defer deinit()
-
-	/*background = sdl.Load("gfx/grid.png")
-	tiles[0] = sdl.Load("gfx/empty.png")
-	tiles[1] = sdl.Load("gfx/o.png")
-	tiles[2] = sdl.Load("gfx/x.png")
-*/
-	/*	if image == nil {
-			panic(sdl.GetError())
-		}
-		defer image.Free()
-
-	*/
-	sdl.WM_SetIcon(tiles[Cross], nil)
 	running := true
 
-	font := ttf.OpenFont("Fontin Sans.otf", 36)
+	font := ttf.OpenFont(path.Join(FONT_DIR, "Fontin Sans.otf"), 36)
 	if font == nil {
 		panic(sdl.GetError())
 	}
@@ -268,7 +292,7 @@ func main() {
 	red := sdl.Color{255, 0, 0, 0}
 	text := &sdl.Surface{}
 	//ttf.RenderText_Blended(font, "Have fun!", red)
-	music := mixer.LoadMUS("test.ogg")
+	music := mixer.LoadMUS(path.Join(MUSIC_DIR, "bgm.ogg"))
 
 	if music == nil {
 		panic(sdl.GetError())
@@ -276,18 +300,11 @@ func main() {
 	defer music.Free()
 
 	music.PlayMusic(-1)
-
-	if sdl.GetKeyName(270) != "[+]" {
-		panic("GetKeyName broken")
-	}
 	ticker := time.NewTicker(1e9 / 50 /*50Hz*/ )
 
 	board := NewGameBoard()
-	tile := TileSymbol(Naught)
+	tile := TileSymbol(Naught) // Naught plays first
 
-	// Note: The following SDL code is highly ineffective.
-	//       It is eating too much CPU. If you intend to use Go-SDL,
-	//       you should to do better than this.
 	for running {
 		select {
 		case <-ticker.C: // Redraw
@@ -299,28 +316,13 @@ func main() {
 			switch e := _event.(type) {
 			case sdl.QuitEvent:
 				running = false
-
 			case sdl.KeyboardEvent:
-				println("")
-				println(e.Keysym.Sym, ": ", sdl.GetKeyName(sdl.Key(e.Keysym.Sym)))
-
 				if e.Keysym.Sym == sdl.K_ESCAPE {
 					running = false
 				}
-
-				fmt.Printf("%04x ", e.Type)
-
-				for i := 0; i < len(e.Pad0); i++ {
-					fmt.Printf("%02x ", e.Pad0[i])
-				}
-				println()
-
-				fmt.Printf("Type: %02x Which: %02x State: %02x Pad: %02x\n", e.Type, e.Which, e.State, e.Pad0[0])
-				fmt.Printf("Scancode: %02x Sym: %08x Mod: %04x Unicode: %04x\n", e.Keysym.Scancode, e.Keysym.Sym, e.Keysym.Mod, e.Keysym.Unicode)
-
 			case sdl.MouseButtonEvent:
 				if e.Type == sdl.MOUSEBUTTONDOWN {
-					println("Trying to place ", tile, "at:", e.X, e.Y)
+					// User clicked somewhere... do we place a tile?
 					if board.PlaceTile(tile, int(e.X), int(e.Y)) {
 						tile.Flip()
 					}
@@ -331,11 +333,10 @@ func main() {
 						fmt.Println(winString)
 					}
 				}
-
 			case sdl.ResizeEvent:
 				println("resize screen ", e.W, e.H)
 
-				screen = sdl.SetVideoMode(int(e.W), int(e.H), 32, sdl.RESIZABLE)
+				screen = sdl.SetVideoMode(int(e.W), int(e.H), 32, sdl.RESIZABLE & sdl.OPENGL)
 
 				if screen == nil {
 					panic(sdl.GetError())
